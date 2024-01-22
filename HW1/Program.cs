fa59a65bf0cf30cc0a5aad5c957834ac6c2e037f
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-01-21 18:16:22 -0800

    Program now reads line

---
+++ HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        GetIntList();
+    }
+
+    static void GetIntList()
+    {
+        Console.WriteLine("Write a list of integer numbers");
+        string line = Console.ReadLine();
+        Console.WriteLine("You wrote " + line);
+    }
+}
