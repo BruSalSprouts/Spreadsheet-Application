3363a84e4216a35ea9624b1b0fdbc6690b9a0470
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-01-23 12:14:41 -0800

    Re-made HW1 Project so it works, got 2nd part done. Added some comments, testing running in Rider IDE

---
+++ HW1/BST.cs
@@ -0,0 +1,34 @@
+namespace HW1;
+
+public class BST
+{
+    public Node root { get; set; }
+
+    public BST(){
+        root = null;
+    }
+    
+    public void Insert(int value)
+    {
+        root = InsertHelp(root, value);
+    }
+
+    private Node InsertHelp(Node branch, int value)
+    {
+        if (branch == null)
+        {
+            branch = new Node(value);
+            return branch;
+        }
+        if (value < branch.content)
+        {
+            branch.leftNode = InsertHelp(branch.leftNode, value);
+        }
+        else if (value > branch.content)
+        {
+            branch.rightNode = InsertHelp(branch.rightNode, value);
+        }
+        return branch;
+    }
+
+}
--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,17 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using HW1;
 
-class Program
+class Program : BST
 {
-    static void Main()
+    public static void Main()
     {
         Console.WriteLine("Hello World!");
-        GetIntList();
+        string line = GetIntList();
+        int len = line.Length;
+        string[] numbersString = line.Split(' '); //Split string into string arrays, which will go into BST
+        BST numbers = new BST();
+        int temp = 0;
+        foreach (string var in numbersString){
+            try
+            {
+                temp = int.Parse(var);
+				numbers.Insert(temp);
+                Console.WriteLine("The new integer is " + temp);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Failed to convert '{var}' to an integer. Format is invalid.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Failed to convert '{var}' to an integer. Value is too large or too small.");
+            }
+        }
+        
+		
     }
-
-    static void GetIntList()
+	/* Prompts user to type in a bunch of integer numbers with spaces in between, and returns the line typed in as a string */
+    public static string GetIntList()
     {
         Console.WriteLine("Write a list of integer numbers");
         string line = Console.ReadLine();
         Console.WriteLine("You wrote " + line);
+        return line;
     }
+
 }
