a7b1b2e13c932804d2b2ef7eff6f7bc3d3a22372
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-01-23 16:02:55 -0800

    Readded files to get things working

---
+++ HW1/Node.cs
@@ -0,0 +1,13 @@
+namespace HW1;
+
+public class Node
+{
+    public Node leftNode { get; set; }
+    public Node rightNode { get; set; }
+    public int content { get; set; }
+    public Node(int newContent){
+        content = newContent;
+        leftNode = null;
+        rightNode = null;
+    }
+}
