1fe6b1372ba71c054d2435cdf2c73682591eda22
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-01-30 16:56:17 -0800

    Set up test case project so it actually tests methods in MainWindowViewModel.cs, Gonna add test cases next

--- HW2-DistinctIntegers/ViewModels/MainWindowViewModel.cs
+++ HW2-DistinctIntegers/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
-﻿namespace HW2_DistinctIntegers.ViewModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace HW2_DistinctIntegers.ViewModels;
+using System;
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
@@ -10,9 +14,36 @@ public class MainWindowViewModel : ViewModelBase
 
     private string RunDistinctIntegers()
     {
-        return "Hello World!";
+        var rand = new Random();
+        List<int> numbers = new List<int>();
+        StringBuilder responseString = new StringBuilder("");
+        for (int i = 0; i < 10000; i++)
+        {
+            numbers.Add(rand.Next(0, 20000));
+        }
+        int numListSize = numbers.Count;
+        
+        
+        //Converting the StringBuild response to a string and returning that
+        // return responseString.ToString();
+        return "Hello World";
     }
 
+    public static int hashMethod(List<int> numbers)
+    {
+        return 0;
+    }
+
+    public static int oOneMethod(List<int> numbers)
+    {
+        return 0;
+    }
+
+    public static int sortingMethod(List<int> numbers)
+    {
+        return 0;
+    }
+    
     public string Greeting { get; set; }
 #pragma warning restore CA1822 // Mark members as static
 }
--- HW2-TestCases/UnitTest1.cs
+++ HW2-TestCases/UnitTest1.cs
@@ -1,5 +1,8 @@
-namespace HW2_TestCases;
+using NUnit.Framework.Interfaces;
 
+namespace HW2_TestCases;
+using NUnit.Framework;
+using HW2_DistinctIntegers.ViewModels;
 public class Tests
 {
     [SetUp]
@@ -10,6 +13,14 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        //Hash method Tests
+        List<int> testList = new List<int>();
+        testList.Add(1);
+        Assert.Zero(MainWindowViewModel.hashMethod(testList));
+
+        //O(1) method Tests
+
+
+        //Sorting method Tests
     }
 }
