3d95e3b24714f2f99a4b928e3f52e5e4c3e22881
Bruno Sanchez <bruno.sanchez.parra@gmail.com>
2024-02-08 15:07:57 -0800

    Test push before trying a fix, deleting obj file

--- TextFileLoaderSaver/ViewModels/ViewModelBase.cs
+++ TextFileLoaderSaver/ViewModels/ViewModelBase.cs
@@ -1,7 +1,16 @@
-﻿using ReactiveUI;
+﻿using System.IO;
+using ReactiveUI;
 
 namespace TextFileLoaderSaver.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
+    public static string fibonacciNumbers()
+    {
+        return "";
+    }
+    public void LoadText(StreamReader textReader)
+    {
+        throw new System.NotImplementedException();
+    }
 }
